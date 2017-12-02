f22fe00123c7c8af2fdd1632c5fa1759b964e86d
Inzamam Malik <malikasinger@gmail.com>
2017-12-02 19:02:57 +0500

    using firebase functions as fulfilment webhook

---
+++ 6 using fulfilment webhook/functions/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.webhook = functions.https.onRequest((request, response) => {
+
+    console.log("request.body.result.parameters: ", request.body.result.parameters);
+
+    let params = request.body.result.parameters;
+
+    response.send({
+        speech:
+            `${params.name} your hotel booking request for ${params.RoomType} room is forwarded for
+             ${params.persons} persons, we will contact you on ${params.email} soon`
+    });
+});
