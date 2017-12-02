9636619123fd33d1bdf71a10de78ce25d413fb17
Inzamam Malik <malikasinger@gmail.com>
2017-12-02 20:06:48 +0500

    minor

--- 6 using fulfilment webhook/functions/index.js
+++ 6 using fulfilment webhook/functions/index.js
@@ -4,14 +4,19 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.webhook = functions.https.onRequest((request, response) => {
-
-    console.log("request.body.result.parameters: ", request.body.result.parameters);
-
-    let params = request.body.result.parameters;
-
-    response.send({
-        speech:
-            `${params.name} your hotel booking request for ${params.RoomType} room is forwarded for
-             ${params.persons} persons, we will contact you on ${params.email} soon`
+    
+        console.log("request.body.result.parameters: ", request.body.result.parameters);
+        // {
+        //     name: "john",
+        //     persons: "3"
+        // }
+    
+        let params = request.body.result.parameters;
+    
+        response.send({
+            speech:
+                `${params.name} your hotel booking request for ${params.RoomType} room is forwarded for
+                 ${params.persons} persons, we will contact you on ${params.email} soon`
+        });
     });
-});
+    
